e6f16e4eec3885c89747b2c8883155e9adca2735
WhiptongueHydra <stephencardwell@protonmail.com>
2025-12-11 17:34:51 +0000

    Update aoc1ZCounter.vhd

--- day1/part1/VHDL/src/aoc1ZCounter.vhd
+++ day1/part1/VHDL/src/aoc1ZCounter.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+
 entity aoc1ZCounter IS
   generic(
     numInstructions: Positive
@@ -13,12 +14,11 @@ entity aoc1ZCounter IS
   );
 end entity aoc1ZCounter;
 
-
+  
 architecture A1 of aoc1ZCounter is
   signal cnt: integer := 0;
 begin
   passwd <= cnt;
-  
   process(clk) begin -- was process(inSum)
     if rising_edge(clk) then
       if (inSum = 0) then
